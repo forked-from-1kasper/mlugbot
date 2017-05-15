7572abc5ed8b27be13c869af7066f9d0f2d0f1dc
1kasper <ktulhu1fhtagn@gmail.com>
2017-05-15 19:31:26 +0700

    Just for lulz

--- russian_parser/__init__.py
+++ russian_parser/__init__.py
@@ -1,3 +1,21 @@
-# WIP. IT'S AMAZING
+# nominative
+# genitive
+# dative
+# accusative
+# instrumental
+# prepositional
+
+
+class Word:
+    def __init__(self, text):
+        self.text = text
+        self.identify()
+
+    def identify(self):
+        pass
+
+
 def parse(text):
+    text = text.split()
+    text = list(map(Word, text))
     return False
